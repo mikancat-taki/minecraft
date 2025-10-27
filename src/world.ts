0056880e2b388137fb6695f718958e6aa1afe173
mikancat10 <216382990+mikancat-taki@users.noreply.github.com>
2025-10-27 23:20:35 +0900

    Update world.ts

--- src/world.ts
+++ src/world.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { createBlock } from './block';
 
 export class World {
     scene: THREE.Scene;
@@ -8,15 +9,16 @@ export class World {
     }
 
     generateFlatWorld() {
-        const geometry = new THREE.BoxGeometry(1,1,1);
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
         for(let x= -10; x<10; x++){
             for(let z= -10; z<10; z++){
-                const block = new THREE.Mesh(geometry, material);
-                block.position.set(x, 0, z);
+                const block = createBlock('grass', new THREE.Vector3(x,0,z));
                 this.scene.add(block);
             }
         }
+    }
+
+    addBlock(pos: THREE.Vector3, type: string) {
+        const block = createBlock(type, pos);
+        this.scene.add(block);
     }
 }
